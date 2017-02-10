2ebdfa561f585266d6fa78722f2e8b11bb78fa5c
João Santos <joaomiguel.mcs@gmail.com>
2017-02-10 21:35:09 +0000

    Changed build process to output typescript sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,34 @@
-var gulp   = require('gulp');
-var sass   = require('gulp-sass');
-var ts     = require('gulp-typescript');
-var tslint = require('gulp-tslint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var bSync  = require('browser-sync').create();
+var gulp        = require('gulp'),
+    concat      = require('gulp-concat'),
+    htmlmin     = require('gulp-htmlmin'),
+    sass        = require('gulp-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    tslint      = require('gulp-tslint'),
+    typescript  = require('gulp-typescript'),
+    uglify      = require('gulp-uglify'),
+    browsersync = require('browser-sync').create();
 
-var tsConfig = ts.createProject('tsconfig.json');
+var tsConfig = typescript.createProject('tsconfig.json');
+
+gulp.task('dependencies', function() {
+    return gulp
+        .src('./node_modules/lz-string/libs/lz-string.min.js')
+        .pipe(gulp.dest('./source/js'))
+        .pipe(gulp.dest('./build/js'))
+        .pipe(gulp.dest('./release/js'));
+});
 
 gulp.task('sass', function() {
     return gulp
-        .src('./src/sass/*.scss')
+        .src('./source/sass/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./build/css'))
-        .pipe(bSync.stream());
+        .pipe(browsersync.stream());
 });
 
 gulp.task('lint', function() {
     return gulp
-        .src('./src/typescript/*.ts')
+        .src('./source/typescript/*.ts')
         .pipe(tslint({
             formatter: "stylish"
         }))
@@ -28,14 +38,24 @@ gulp.task('lint', function() {
 gulp.task('typescript', ['lint'], function() {
     var result = tsConfig
         .src()
+        .pipe(sourcemaps.init())
         .pipe(tsConfig());
 
-    return result.js.pipe(gulp.dest('./build/js'));
+    return result.js
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('watch:typescript', ['typescript'], function(done) {
-    bSync.reload();
+    browsersync.reload();
     done();
+});
+
+gulp.task('release:html', function() {
+    return gulp
+        .src('./build/index.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./release'));
 });
 
 gulp.task('release:css', function() {
@@ -55,18 +75,18 @@ gulp.task('release:js', function() {
         .pipe(gulp.dest('./release/js'));
 });
 
-gulp.task('release', ['release:css', 'release:js']);
+gulp.task('release', ['dependencies', 'release:html', 'release:css', 'release:js']);
 
-gulp.task('run', ['sass', 'typescript'], function() {
-    bSync.init({
+gulp.task('run', ['dependencies', 'sass', 'typescript'], function() {
+    browsersync.init({
         server: {
             baseDir: './build'
         }
     });
 
-    gulp.watch('./build/index.html').on('change', bSync.reload);
-    gulp.watch('./src/sass/*.scss', ['sass']);
-    gulp.watch('./src/typescript/*.ts', ['watch:typescript']);
+    gulp.watch('./build/index.html').on('change', browsersync.reload);
+    gulp.watch('./source/sass/*.scss', ['sass']);
+    gulp.watch('./source/typescript/*.ts', ['watch:typescript']);
 });
 
 gulp.task('default', ['run']);
